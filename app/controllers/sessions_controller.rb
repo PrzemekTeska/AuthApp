c16a7a286abfc5bbf6349fab6133ed14c5f0b506
PrzemekTeska <przemkooo98@interia.pl>
2019-12-20 02:25:16 +0100

    Kilka poprawek

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   include CurrentUserConcern
+
   def create
     user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
 
@@ -12,6 +13,7 @@ class SessionsController < ApplicationController
       }
     else
       render json: { status: 401 }
+    end
   end
 
   def logged_in
@@ -22,7 +24,7 @@ class SessionsController < ApplicationController
       }
     else
       render json: {
-        logged_in: false,
+        logged_in: false
       }
   end
 end
